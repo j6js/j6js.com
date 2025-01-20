82e259226dbacb5c5499eda621bb9ec80e2d601d
Jackson Kelly <wild.dives.0x@icloud.com>
2025-01-20 20:36:32 +1300

    Add Noto Sans to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,47 +1,55 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		animation: {
-  			'background-gradient': 'background-gradient var(--background-gradient-speed, 15s) cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite'
-  		},
-  		keyframes: {
-  			'background-gradient': {
-  				'0%, 100%': {
-  					transform: 'translate(0, 0)',
-  					animationDelay: 'var(--background-gradient-delay, 0s)'
-  				},
-  				'20%': {
-  					transform: 'translate(calc(100% * var(--tx-1, 1)), calc(100% * var(--ty-1, 1)))'
-  				},
-  				'40%': {
-  					transform: 'translate(calc(100% * var(--tx-2, -1)), calc(100% * var(--ty-2, 1)))'
-  				},
-  				'60%': {
-  					transform: 'translate(calc(100% * var(--tx-3, 1)), calc(100% * var(--ty-3, -1)))'
-  				},
-  				'80%': {
-  					transform: 'translate(calc(100% * var(--tx-4, -1)), calc(100% * var(--ty-4, -1)))'
-  				}
-  			}
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        noto: ["var(--font-noto-sans)", "sans-serif"],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      animation: {
+        "background-gradient":
+          "background-gradient var(--background-gradient-speed, 15s) cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite",
+      },
+      keyframes: {
+        "background-gradient": {
+          "0%, 100%": {
+            transform: "translate(0, 0)",
+            animationDelay: "var(--background-gradient-delay, 0s)",
+          },
+          "20%": {
+            transform:
+              "translate(calc(100% * var(--tx-1, 1)), calc(100% * var(--ty-1, 1)))",
+          },
+          "40%": {
+            transform:
+              "translate(calc(100% * var(--tx-2, -1)), calc(100% * var(--ty-2, 1)))",
+          },
+          "60%": {
+            transform:
+              "translate(calc(100% * var(--tx-3, 1)), calc(100% * var(--ty-3, -1)))",
+          },
+          "80%": {
+            transform:
+              "translate(calc(100% * var(--tx-4, -1)), calc(100% * var(--ty-4, -1)))",
+          },
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
