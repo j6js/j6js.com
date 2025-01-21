e6d01f46c651a8059b6e312fe6ec96802652e05b
Jackson Kelly <wild.dives.0x@icloud.com>
2025-01-21 21:21:25 +1300

    Add delays to account for loading

--- components/intro.tsx
+++ components/intro.tsx
@@ -16,12 +16,12 @@ export default function Intro() {
         <motion.div
           initial={{ filter: "blur(13px)" }}
           animate={{ filter: "blur(0px)" }}
-          transition={{ duration: 0.9, ease: "easeOut" }}
+          transition={{ duration: 0.9, ease: "easeOut", delay: 0.9 }}
         >
           <motion.div
             initial={{ marginRight: "0px", scale: 0.75 }}
             animate={{ marginRight: "44px", scale: 1 }}
-            transition={{ delay: 0.2, duration: 1, ease: "easeInOut" }}
+            transition={{ delay: 1.1, duration: 1, ease: "easeInOut" }}
           >
             <Image
               src="/profile.png"
@@ -36,12 +36,12 @@ export default function Intro() {
         <motion.div
           initial={{ filter: "blur(10px)" }}
           animate={{ filter: "blur(0px)" }}
-          transition={{ duration: 0.9, ease: "easeOut" }}
+          transition={{ duration: 0.9, ease: "easeOut", delay: 0.9 }}
         >
           <motion.div
             initial={{ scaleX: 0.09, scaleY: 0.00001 }}
             animate={{ scaleX: 1, scaleY: 1 }}
-            transition={{ delay: 0.15, duration: 1, ease: "easeInOut" }}
+            transition={{ delay: 1.05, duration: 1, ease: "easeInOut" }}
           >
             <BreathingText
               label="Jackson Kelly"
@@ -61,54 +61,60 @@ export default function Intro() {
         <div className="absolute bottom-0 left-0 right-0 flex justify-center">
           <div>
             <motion.div
-              initial={{ translateY: 2 }}
-              animate={{ translateY: 3 }}
-              transition={{
-                repeat: Infinity,
-                duration: 1,
-                repeatType: "reverse",
-                ease: "easeInOut",
-                delay: 1,
-              }}
+              initial={{ filter: "blur(10px)", scale: 0.8 }}
+              animate={{ filter: "blur(0px)", scale: 1 }}
+              transition={{ delay: 0.9, duration: 1 }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                x="0px"
-                y="-10px"
-                viewBox="0 0 72 90"
-                enableBackground="new 0 0 72 72"
-                className="fill-slate-900 dark:fill-yellow-200 w-24 h-24 translate-y-20"
+              <motion.div
+                initial={{ translateY: 2 }}
+                animate={{ translateY: 3 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 1,
+                  repeatType: "reverse",
+                  ease: "easeInOut",
+                  delay: 1.9,
+                }}
               >
-                <g>
-                  <polygon points="36,43.7 27.6,35.2 25.4,37.4 36,47.9 46.6,37.3 44.4,35.2  " />
-                </g>
-              </svg>
-            </motion.div>
-            <motion.div
-              initial={{ translateY: 20 }}
-              animate={{ translateY: -0.41 }}
-              transition={{
-                repeat: Infinity,
-                duration: 1,
-                repeatType: "reverse",
-                ease: "easeInOut",
-                delay: 1,
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 72 90"
-                enableBackground="new 0 0 72 72"
-                className="fill-slate-900 dark:fill-yellow-200 w-24 h-24"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  x="0px"
+                  y="-10px"
+                  viewBox="0 0 72 90"
+                  enableBackground="new 0 0 72 72"
+                  className="fill-slate-900 dark:fill-yellow-200 w-24 h-24 translate-y-20"
+                >
+                  <g>
+                    <polygon points="36,43.7 27.6,35.2 25.4,37.4 36,47.9 46.6,37.3 44.4,35.2  " />
+                  </g>
+                </svg>
+              </motion.div>
+              <motion.div
+                initial={{ translateY: 20 }}
+                animate={{ translateY: -0.41 }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 1,
+                  repeatType: "reverse",
+                  ease: "easeInOut",
+                  delay: 1.9,
+                }}
               >
-                <g>
-                  <polygon points="46.6,27 44.4,24.9 36,33.3 27.6,24.9 25.4,27 36,37.5  " />
-                </g>
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 72 90"
+                  enableBackground="new 0 0 72 72"
+                  className="fill-slate-900 dark:fill-yellow-200 w-24 h-24"
+                >
+                  <g>
+                    <polygon points="46.6,27 44.4,24.9 36,33.3 27.6,24.9 25.4,27 36,37.5  " />
+                  </g>
+                </svg>
+              </motion.div>
             </motion.div>
           </div>
         </div>
